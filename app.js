4e76779ba9c4428f60fe595e35d8efed2e985c7c
Alex Hipolito <alexheepo@gmail.com>
2021-07-14 22:23:32 +1000

    Set up route to about page. Update data.json

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
 
 const mainRoutes = require('./routes');
+const aboutRoute = require('./routes/about');
 
 app.use(mainRoutes);
+app.use('/about', aboutRoute);
 
 app.listen(3000, () => {
     console.log('The application is running on localhost: 3000!')
