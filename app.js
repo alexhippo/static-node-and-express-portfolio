dda998e50f531d9efb5b02d14e66026a5dc82ce6
Alex Hipolito <alexheepo@gmail.com>
2021-07-14 21:59:30 +1000

    Setup server and middleware. Set up route for index page

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { data } = require('./data.json');
+
+const app = express();
+
+app.use('/static', express.static('public'));
+
+app.set('view engine', 'pug');
+
+const mainRoutes = require('./routes');
+
+app.use(mainRoutes);
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost: 3000!')
+});
