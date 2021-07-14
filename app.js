565b87813cb4b79aa64f444615542f90dad4a321
Alex Hipolito <alexheepo@gmail.com>
2021-07-14 22:43:54 +1000

    Set up dynamic project routes

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ app.set('view engine', 'pug');
 
 const mainRoutes = require('./routes');
 const aboutRoute = require('./routes/about');
+const projectRoutes = require('./routes/projects');
 
 app.use(mainRoutes);
 app.use('/about', aboutRoute);
+app.use('/projects', projectRoutes);
 
 app.listen(3000, () => {
     console.log('The application is running on localhost: 3000!')
