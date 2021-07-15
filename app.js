5149264d1376a15d420b2accc53d541d695ad825
Alex Hipolito <alexheepo@gmail.com>
2021-07-15 18:49:46 +1000

    Handle errors on root and project routes

--- app.js
+++ app.js
@@ -14,6 +14,36 @@ app.use(mainRoutes);
 app.use('/about', aboutRoute);
 app.use('/projects', projectRoutes);
 
+// Create and throw 500 error on route
+app.use('/error', (req, res, next) => {
+    const err = new Error();
+    err.status = 500;
+    throw err;
+});
+
+app.use((req, res, next) => {
+    const err = new Error(`Sorry, we couldn't find that page.`);
+    console.log(err.message);
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+
+    if (err.status === 404) {
+        res.status(404);
+        res.render('error', err);
+        console.log(err.message);
+    } else {
+        err.message = err.message || "Sorry, something went wrong. Please try again later.";
+        res.status(err.status || 500).render('error', err);
+        console.log(err.message);
+    }
+});
+
+
+
 app.listen(3000, () => {
     console.log('The application is running on localhost: 3000!')
 });
---
+++ routes/projects.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const { projects } = require('../data.json');
+
+
+router.get('/', (req, res) => {
+    const randomProjectNumber = Math.floor(Math.random() * (projects.length - 1));
+    res.redirect(`/projects/${randomProjectNumber}`);
+});
+
+// Create and throw 500 error on project
+router.get('/error', (req, res, next) => {
+    const err = new Error();
+    err.message = `Sorry, something went wrong loading this project. Please try again later.`
+    err.status = 500;
+    throw err;
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    if (projects[id]) {
+        res.render('project', projects);
+    } else {
+        const err = new Error(`Sorry, we couldn't find that project.`);
+        err.status = 404;
+        throw err;
+    }
+});
+
+module.exports = router;
