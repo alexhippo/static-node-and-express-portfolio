354deb0d613a981c4222fa8d35ec9e74e4adf384
Alex Hipolito <alexheepo@gmail.com>
2021-07-17 16:16:55 +1000

    Refactoring route and error handlers

--- app.js
+++ app.js
@@ -1,46 +1,36 @@
 const express = require('express');
-
 const app = express();
 
 app.use('/static', express.static('public'));
-
 app.set('view engine', 'pug');
 
-const mainRoutes = require('./routes');
+const mainRoute = require('./routes');
 const aboutRoute = require('./routes/about');
 const projectRoutes = require('./routes/projects');
 
-app.use('/static', express.static('public'));
-
-app.use(mainRoutes);
+app.use(mainRoute);
 app.use('/about', aboutRoute);
 app.use('/projects', projectRoutes);
 
-// Create and throw 500 error on route
-app.use('/error', (req, res, next) => {
-    const err = new Error();
-    err.status = 500;
-    throw err;
-});
-
+// 404 Error Handler
 app.use((req, res, next) => {
     const err = new Error(`Sorry, we couldn't find that page.`);
-    console.log(err.message);
     err.status = 404;
+    console.log(`Error Status ${err.status}: ${err.message}`);
     next(err);
 });
 
+// Global Error Handler
 app.use((err, req, res, next) => {
     res.locals.error = err;
 
     if (err.status === 404) {
-        res.status(404);
-        res.render('page-not-found', err);
-        console.log(err.message);
+        res.status(404).render('page-not-found', err);
     } else {
         err.message = err.message || "Sorry, something went wrong. Please try again later.";
+        err.status = err.status || 500;
+        console.log(`Error Status ${err.status}: ${err.message}`);
         res.status(err.status || 500).render('error', err);
-        console.log(err.message);
     }
 });
 
--- routes/projects.js
+++ routes/projects.js
@@ -2,21 +2,13 @@ const express = require('express');
 const router = express.Router();
 const { projects } = require('../data.json');
 
-
+// Display a random project if user goes to /projects only route
 router.get('/', (req, res) => {
     const randomProjectNumber = Math.floor(Math.random() * (projects.length - 1));
     res.redirect(`/projects/${randomProjectNumber}`);
 });
 
-// Create and throw 500 error on project
-router.get('/error', (req, res, next) => {
-    const err = new Error();
-    err.message = `Sorry, something went wrong loading this project. Please try again later.`
-    err.status = 500;
-    throw err;
-});
-
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res, next) => {
     const { id } = req.params;
 
     if (projects[id]) {
@@ -24,7 +16,8 @@ router.get('/:id', (req, res) => {
     } else {
         const err = new Error(`Sorry, we couldn't find that project.`);
         err.status = 404;
-        throw err;
+        console.log(`Error Status ${err.status}: ${err.message}`);
+        next(err);
     }
 });
 
